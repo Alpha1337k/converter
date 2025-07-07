478451f88186f3eb93057c280e08f037fe97b3c5
Alpha1337k <oscar@kruithof.io>
2025-07-07 15:19:47 +0200

    Feat: made conversion more cli friendly

---
+++ src/constants.rs
@@ -0,0 +1,3 @@
+
+pub const CONVERTER_CONFIG_DIR: &str ="/mnt/wsl/PHYSICALDRIVE0p1/projects/converter";
+pub const LOADING_ANIMATION: [&str; 6] = ["⠏", "⠛", "⠹", "⠼", "⠶", "⠧"];
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,50 @@
-use std::{collections::HashSet, ffi::{OsString}, path::{PathBuf}, fmt::Display, process::exit};
+use std::{collections::HashSet, ffi::OsString, fmt::Display, path::{Path, PathBuf}, process::exit};
 
 use console::{style};
 use dialoguer::{theme::ColorfulTheme, FuzzySelect, Input, Confirm, Editor};
 use glob::{glob};
 
-pub mod converters;
-pub mod file_types;
+mod converters;
+mod file_types;
+mod prompts;
+mod constants;
 
-use crate::{converters::{get_converters, Converter, run_converter}, file_types::{get_file_type, get_file_types_flat}};
+use crate::{converters::{find_converter, get_converters, run_converter, Converter}, file_types::{get_file_type, get_file_types_flat}, prompts::{basic_prompt, confirm_prompt, select}};
+use clap::Parser;
+use thiserror::Error;
 
-fn select<T: Display>(prompt: &str, options: &Vec<T>) -> usize {
-    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
-        .with_prompt(prompt)
-		.max_length(5_usize)
-        .default(0)
-        .items(&options[..])
-        .interact()
-        .unwrap();
+#[derive(Error, Debug, Clone, PartialEq)]
+pub enum ArgError {
+	#[error("Not enough arguments provided: {len}. Need at least two.")]
+	InvalidArgumentCount { len: usize },
 
-	return selection;
+	#[error("Unrecognized file extension for '{file}'.")]
+	UnrecognizedExtension { file: PathBuf },
 }
 
-fn basic_prompt(query: &str) -> String {
-	return Input::with_theme(&ColorfulTheme::default())
-        .with_prompt(query)
-        .interact_text()
-        .unwrap();
+#[derive(Error, Debug, Clone, PartialEq)]
+pub enum RunError {
+	#[error("Could not find a possible conversion from {in_ext} to {out_ext}.")]
+	NoConversionPossible {in_ext: String, out_ext: String}
 }
 
-fn confirm_prompt(query: &str) -> bool {
-	let confirmed = Confirm::with_theme(&ColorfulTheme::default())
-		.with_prompt(query)
-		.interact()
-		.unwrap();
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+	input_file: PathBuf,
+	output_file: PathBuf,
 
-	return confirmed;
+	#[arg(short, long)]
+	edit: bool,
+
+	// #[arg(short, long)]
+	// yes: bool,
+
+	// #[arg(short, long)]
+	// input_type: Option<String>,
+
+	// #[arg(short, long)]
+	// output_type: Option<String>,
 }
 
 fn get_target_files(raw_glob: &str) -> Vec<PathBuf> {
@@ -47,7 +57,7 @@ fn get_target_files(raw_glob: &str) -> Vec<PathBuf> {
 	return files
 }
 
-fn get_extension(files: &Vec<PathBuf>) -> Option<String> {
+fn get_extensions(files: &Vec<PathBuf>) -> Option<String> {
 	let mut extensions = HashSet::new();
 
 	for file in files {
@@ -68,7 +78,7 @@ fn get_extension(files: &Vec<PathBuf>) -> Option<String> {
 fn get_filetype(files: &Vec<PathBuf>) -> String {
 	let _file_type = String::new();
 
-	match get_extension(&files) {
+	match get_extensions(&files) {
 		Some(ext) => 'autoext: {
 			let file_types = get_file_type(&ext);
 			if file_types.len() == 0 {
@@ -94,60 +104,118 @@ fn get_filetype(files: &Vec<PathBuf>) -> String {
 	return types[type_idx].clone();
 }
 
+
+fn populate_output_type(positionals: &Vec<String>) -> Result<String, ArgError> {
+	if positionals.len() < 2 {
+		return Err(ArgError::InvalidArgumentCount { len: positionals.len() });
+	}
+	
+	let last = positionals.last().unwrap();
+
+	let extension = match last.split(".").last() {
+		Some(s) => Ok(s.to_string()),
+		None => Err(ArgError::UnrecognizedExtension { file: last.to_string().into() })
+	}?;
+
+
+	Ok(extension)
+}
+
 fn main() {
-	let converters = get_converters();
-	let target_glob = basic_prompt("[GLOB] Select target files: ");
-	let files = get_target_files(&target_glob);
+	let args = Args::parse();
 
-	if files.len() == 0 {
-		println!("Error: no files found matching {}.", &target_glob);
-		exit(1)
-	}
+	dbg!(&args);
 
-	println!("{}{}{}", style("Found ").dim(), style(files.len()).bold(), style(" files.").dim());
-	let file_type = get_filetype(&files);
-
-	let mut selected_converter_tmp: Option<Converter> = None;
-
-	for converter in converters {
-		if converter.convert_from[&file_type].is_string() {
-			selected_converter_tmp = Some(converter);
-			break;
+	let loaded_converters = match get_converters() {
+		Ok(v) => v,
+		Err(e) => {
+			eprintln!("{:?}", e);
+			exit(1)
 		}
-	}
+	};
 
-	let selected_converter = selected_converter_tmp.unwrap_or_else(|| {
-		println!("Failed to find converter for this filetype.");
-		exit(1);
-	});
+	dbg!(&loaded_converters);
 
-	let selections: Vec<&String> = selected_converter
-		.convert_to.as_object()
-		.unwrap()
-		.iter()
-		.map(|v| v.0)
-		.collect()	
-		;
+	let input_extension = match args.input_file.extension()
+		.and_then(|v| v.to_str()) {
+		Some(ext) => ext,
+		None => {
+			eprintln!("Error: {}", ArgError::UnrecognizedExtension { file: args.input_file });
+			std::process::exit(1);
+		}
+	};
 
-	let target_output = selections[select("Convert to:",&selections)];
+	let output_extension = match args.output_file.extension() 
+		.and_then(|v| v.to_str()) {
+		Some(ext) => ext,
+		None => {
+			eprintln!("Error: {}", ArgError::UnrecognizedExtension { file: args.output_file });
+			std::process::exit(1);
+		}
+	};
 
-	let needs_cmd_edit = confirm_prompt("Do you want to add parameters?");
+	let selected_converter = match find_converter( &loaded_converters, input_extension, output_extension) {
+		Some(c) => c,
+		None => {
+			eprintln!("Error: {}", RunError::NoConversionPossible { in_ext: input_extension.to_string(), out_ext: output_extension.to_string() });
+			exit(1)
+		}
+	};
 
 	let mut prompt = selected_converter.args.clone();
 
-	if needs_cmd_edit {
+	if args.edit {
 		prompt = Editor::new().edit(&prompt).unwrap().unwrap();
 	}
 
-	for file in &files {
-		run_converter(
-			&selected_converter,
-			&prompt,
-			file.as_path().to_str().unwrap(), 
-			&(String::new() + file.file_stem().unwrap().clone().to_str().expect("") + "." + &target_output),
-			&file_type,
-			&target_output)
-	}
+	run_converter(&selected_converter, 
+		&prompt,
+		args.input_file.to_str().unwrap(),
+		args.output_file.to_str().unwrap(),
+		input_extension,
+		output_extension);
+
+	// let mut selected_converter_tmp: Option<Converter> = None;
+
+	// for converter in converters {
+	// 	if converter.convert_from[&file_type].is_string() {
+	// 		selected_converter_tmp = Some(converter);
+	// 		break;
+	// 	}
+	// }
+
+	// let selected_converter = selected_converter_tmp.unwrap_or_else(|| {
+	// 	println!("Failed to find converter for this filetype.");
+	// 	exit(1);
+	// });
+
+	// let selections: Vec<&String> = selected_converter
+	// 	.convert_to.as_object()
+	// 	.unwrap()
+	// 	.iter()
+	// 	.map(|v| v.0)
+	// 	.collect()	
+	// 	;
+
+	// let target_output = selections[select("Convert to:",&selections)];
+
+	// let needs_cmd_edit = confirm_prompt("Do you want to add parameters?");
+
+	// let mut prompt = selected_converter.args.clone();
+
+	// if needs_cmd_edit {
+	// 	prompt = Editor::new().edit(&prompt).unwrap().unwrap();
+	// }
+
+	// for file in &files {
+	// 	run_converter(
+	// 		&selected_converter,
+	// 		&prompt,
+	// 		file.as_path().to_str().unwrap(), 
+	// 		&(String::new() + file.file_stem().unwrap().to_str().expect("") + "." + &target_output),
+	// 		&file_type,
+	// 		&target_output)
+	// }
 
 
 }
