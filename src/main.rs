c8f38424808daabe9c6b4068375c8563bc275dc5
Alpha1337k <oscar@kruithof.io>
2025-07-10 02:13:07 +0200

    Chore: refactor

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,17 @@
 use converter::{
     converters::{find_converter, get_converters},
     converting::run_converter,
-    file_types::{FileTypeError, FileTypes},
-    prompts::{confirm_prompt, select},
+    error_exit,
+    extension::get_extension,
+    file_types::FileTypes,
+    prompts::confirm_prompt,
 };
 use dialoguer::Editor;
 use env_logger::Builder;
-use log::{debug, log_enabled, Log};
-use std::{
-    fmt::{Debug, Display},
-    fs,
-    path::{Path, PathBuf},
-    process::exit,
-};
+use std::{fmt::Debug, fs, path::PathBuf, process::exit};
 
 use clap::Parser;
 use thiserror::Error;
-
-#[derive(Error, Debug, Clone, PartialEq)]
-pub enum ArgError {
-    #[error("Not enough arguments provided: {len}. Need at least two.")]
-    InvalidArgumentCount { len: usize },
-
-    #[error("Unrecognized file extension for '{file}'.")]
-    UnrecognizedExtension { file: PathBuf },
-
-    #[error("Invalid filename.")]
-    InvalidFileName,
-}
 
 #[derive(Error, Debug, Clone, PartialEq)]
 pub enum RunError {
@@ -57,65 +41,6 @@ struct Args {
 
     // #[arg(short, long)]
     // output_type: Option<String>,
-}
-
-fn error_exit<T: Display>(e: T, extra_message: Option<&str>) -> ! {
-    eprintln!("Error: {}{}", extra_message.unwrap_or_default(), e);
-    if !log_enabled!(log::Level::Error) {
-        eprintln!("Hint: Try running the command again with -v for further debugging");
-    }
-    exit(1)
-}
-
-fn get_correct_extension(
-    use_default_format: bool,
-    target: &Path,
-    file_types: &FileTypes,
-    ext: String,
-    prompt: String,
-) -> Result<String, FileTypeError> {
-    let types = file_types.get_file_types(ext)?;
-
-    if types.len() == 1 {
-        return Ok(types[0].clone());
-    }
-
-    if use_default_format {
-        debug!("Using default format for {:?}", target);
-        return Ok(types[0].clone());
-    }
-
-    let idx = select(&prompt, &types);
-
-    Ok(types[idx].clone())
-}
-
-fn get_extension(use_default_format: bool, target: &Path, file_types: &FileTypes) -> String {
-    let prompt = format!(
-        "Multiple filetypes available. Please specify type for {:?}",
-        &target.as_os_str()
-    );
-
-    match target
-        .extension()
-        .and_then(|v| v.to_str())
-        .map(|v| v.to_lowercase())
-        .map(|v| get_correct_extension(use_default_format, &target, file_types, v, prompt))
-    {
-        Some(ext) => match ext {
-            Ok(e) => e,
-            Err(e) => error_exit(e, None),
-        },
-        None => {
-            eprintln!(
-                "{}",
-                ArgError::UnrecognizedExtension {
-                    file: target.into()
-                }
-            );
-            exit(1);
-        }
-    }
 }
 
 fn main() {
@@ -163,7 +88,7 @@ fn main() {
             Ok(v) => {
                 let mut value = v.unwrap_or_default();
 
-                if value == "" {
+                if value.is_empty() {
                     eprintln!("Using default prompt");
                     value = prompt.clone()
                 }
@@ -174,7 +99,7 @@ fn main() {
         }
     }
 
-    if !args.yes && fs::exists(&args.output_file).is_ok_and(|x| x == true) {
+    if !args.yes && fs::exists(&args.output_file).is_ok_and(|x| x) {
         confirm_prompt(&format!(
             "{:?} already exists. Do you want to overwrite this file?",
             &args.output_file
